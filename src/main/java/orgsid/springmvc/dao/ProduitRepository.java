ff98e780f9b8c6793911d6a7b085fb55c758b040
yassine sahouane <sahouaneyassine1999@gmail.com>
2020-07-08 01:06:10 +0100

    'final crud'

--- src/main/java/orgsid/springmvc/dao/ProduitRepository.java
+++ src/main/java/orgsid/springmvc/dao/ProduitRepository.java
@@ -2,9 +2,11 @@ package orgsid.springmvc.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import orgsid.springmvc.entities.Produit;
 @RepositoryRestController
+
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
 	
 
--- src/main/java/orgsid/springmvc/services/ProduitService.java
+++ src/main/java/orgsid/springmvc/services/ProduitService.java
@@ -3,12 +3,14 @@ package orgsid.springmvc.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
+
 import orgsid.springmvc.entities.*;
 import orgsid.springmvc.dao.ProduitRepository;
 
 @RestController
+@CrossOrigin("*")
 public class ProduitService {
 	@Autowired
 	private  ProduitRepository produitRepository ;
@@ -16,5 +18,40 @@ public class ProduitService {
 	public List<Produit> getProduits(){
 		return produitRepository.findAll();
 	}
+	@PostMapping(path = "/listproduit")
+	public Produit addProduit(@RequestBody Produit p){
+		return produitRepository.save(p);
+	}
+	@PutMapping (value = "/listproduit/{id}")
+    public Produit update(@PathVariable(name = "id") Long id,@RequestBody Produit p){
+		
+        return  produitRepository.findById(id).map(produit -> {
+        	produit.setPrixunitaire(p.getPrixunitaire());
+        	produit.setQuantite(p.getQuantite());
+        	produit.setRef(p.getRef());
+        	return produitRepository.save(produit);
+        }).orElseGet(()->{
+        	p.setId(id);
+        	return produitRepository.save(p);
+        	
+        });
+        	    
+        		
+        		
+        		
+        		
+
+    }
+	@DeleteMapping (value = "/listproduit/{id}")
+    public void delete(@PathVariable(name = "id") Long id){
+        produitRepository.deleteById(id);
+
+    }
+	@GetMapping(value = "/listproduit/{id}")
+    public Produit listProduits(@PathVariable(name = "id") Long id){
+        return produitRepository.findById(id).get();
 
 }
+}
+
+
